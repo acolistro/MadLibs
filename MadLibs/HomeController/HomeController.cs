5382d702bfc5ee7bc88084a90eab9507d300968d
acolistro <arcolistro@gmail.com>
2020-03-09 15:33:55 -0700

    add controllers to successfully test routes.

---
+++ MadLibs/HomeController/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Madlibs.Controllers
+{
+  public class HomeController : Controller
+  {
+    public string Hello() { return "Hello friend!"; }
+
+    public string Goodbye() { return "Goodbye friend."; }
+  }   
+}
