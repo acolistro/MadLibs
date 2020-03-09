f2ea121ac08c324841f5298b969b5f0a188fdafe
acolistro <arcolistro@gmail.com>
2020-03-09 16:11:48 -0700

    add model for form variables.

---
+++ MadLibs/Models/MadLibVariable.cs
@@ -0,0 +1,20 @@
+namespace MadLibs.Models
+{
+    public class MadLibVariable
+    {
+        public string Color { get; set; }
+        public string Color2 { get; set; }
+        public string Adjective { get; set; }
+        public string Animals { get; set; }
+        public string Adjective2 { get; set; }
+        public string Animals2 { get; set; }
+        public string Animals3 { get; set; }
+        public string Color3 { get; set; }
+        public string Noun { get; set; }
+        public string Color4 { get; set; }
+        public string Adjective3 { get; set; }
+        public string Color5 { get; set; }
+        public string Verb2 { get; set; }
+        public string Adjective4 { get; set; }
+    }
+}
