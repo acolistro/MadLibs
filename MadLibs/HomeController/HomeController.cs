96bf5d18036691022750ba008031175b2f79274e
acolistro <arcolistro@gmail.com>
2020-03-09 16:27:55 -0700

    add content to story view.

--- MadLibs/HomeController/HomeController.cs
+++ MadLibs/HomeController/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
 
 namespace Madlibs.Controllers
 {
@@ -7,5 +8,22 @@ namespace Madlibs.Controllers
     public string Hello() { return "Hello friend!"; }
 
     public string Goodbye() { return "Goodbye friend."; }
+    [Route("/")]
+    public ActionResult MadLib() { 
+        MadLibVariable myMadLibVariable = new MadLibVariable();
+        myMadLibVariable.Recipient = "Lina";
+        myMadLibVariable.Sender = "Jasmine";
+        return View(myMadLibVariable);
+    }
+    [Route("/form")]
+    public ActionResult SpringMLForm() { return View(); }
+    [Route("/Story")]
+    public ActionResult Story(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }   
 }
