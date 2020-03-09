2f53992af545a9028b3ad33d557d9cb46ac8ef62
acolistro <arcolistro@gmail.com>
2020-03-09 16:43:22 -0700

    add more content to story view.

--- MadLibs/HomeController/HomeController.cs
+++ MadLibs/HomeController/HomeController.cs
@@ -15,15 +15,27 @@ namespace Madlibs.Controllers
         myMadLibVariable.Sender = "Jasmine";
         return View(myMadLibVariable);
     }
-    [Route("/form")]
+    [Route("/springmlform")]
     public ActionResult SpringMLForm() { return View(); }
-    [Route("/Story")]
-    public ActionResult Story(string recipient, string sender)
+    [Route("/story")]
+    public ActionResult Story(string color, string color2, string adjective, string animals, string adjective2, string animals2, string animals3, string color3, string noun, string color4, string adjective3, string color5, string verb2, string adjective4)
     {
-      LetterVariable myLetterVariable = new LetterVariable();
-      myLetterVariable.Recipient = recipient;
-      myLetterVariable.Sender = sender;
-      return View(myLetterVariable);
+      MadLibVariable myMadLibVariable = new MadLibVariable();
+      myMadLibVariable.Color = color;
+      myMadLibVariable.Color2 = color2;
+      myMadLibVariable.Adjective = adjective;
+      myMadLibVariable.Animals = animals;
+      myMadLibVariable.Adjective2 = adjective2;
+      myMadLibVariable.Animals2 = animals2;
+      myMadLibVariable.Animals3 = animals3;
+      myMadLibVariable.Color3 = color3;
+      myMadLibVariable.Noun = noun;
+      myMadLibVariable.Color4 = color4;
+      myMadLibVariable.Adjective3 = adjective3;
+      myMadLibVariable.Color5 = color5;
+      myMadLibVariable.Verb2 = verb2;
+      myMadLibVariable.Adjective4 = adjective4;
+      return View(myMadLibVariable);
     }
   }   
 }
