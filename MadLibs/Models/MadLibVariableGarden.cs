e81803dc7cda4c36b5b0897a740460d05e833b4a
acolistro <arcolistro@gmail.com>
2020-03-09 19:03:25 -0700

    add model for second madlib.

---
+++ MadLibs/Models/MadLibVariableGarden.cs
@@ -0,0 +1,15 @@
+namespace MadLibs.Models
+{
+    public class MadLibVariableGarden
+    {
+        public string Noun { get; set; }
+        public string Adjective { get; set; }
+        public string Noun2 { get; set; }
+        public string Noun3 { get; set; }
+        public string Noun4 { get; set; }
+        public string Noun5 { get; set; }
+        public string Tool { get; set; }
+        public string Noun6 { get; set; }
+        public string Adjective2 { get; set; }
+    }
+}
